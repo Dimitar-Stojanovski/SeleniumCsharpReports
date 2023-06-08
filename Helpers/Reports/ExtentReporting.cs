2c63da3b3c5e4b9acc4e787d9f8269962a2b59ed
Dimitar Stojanovski <78984818+Dimitar-Stojanovski@users.noreply.github.com>
2023-06-08 14:43:37 +0200

    Update ExtentReporting.cs
    Updating the logic for Extent reporting

--- Helpers/Reports/ExtentReporting.cs
+++ Helpers/Reports/ExtentReporting.cs
@@ -15,10 +15,11 @@ namespace SeleniumCsharpReports.Helpers.Reports
         private static ExtentReports extentReports;
         private static ExtentTest extentTest;
 
-        private static ExtentReports StartReporting()
+       private static ExtentReports StartReporting()
         {
-            var path = Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
-            if (extentReports==null)
+            var path = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + @"..\..\..\..\ReportResults\";
+            
+            if (extentReports == null)
             {
                 Directory.CreateDirectory(path);
                 extentReports = new ExtentReports();
@@ -32,11 +33,53 @@ namespace SeleniumCsharpReports.Helpers.Reports
         public static void CreateTest(string _name)
         {
             extentTest = StartReporting().CreateTest(_name);
+           
         }
 
-        public static void EndReporting()
+        public static void EndReporting(string info, string image)
         {
+            EndTest(info, image);
             StartReporting().Flush();
+        }
+
+        
+
+        private static void LogFail(string info)
+        {
+            extentTest.Fail(info);
+        }
+
+        private static void Loginfo(string info)
+        {
+            extentTest.Info(info);
+        }
+
+        private static void LogScreenshot(string info,string image)
+        {
+            extentTest.Info(info, MediaEntityBuilder.CreateScreenCaptureFromBase64String(image).Build());
+        }
+
+        private static void EndTest(string info, string screenshot)
+        {
+            var testStatus = TestContext.CurrentContext.Result.Outcome.Status;
+            var message = TestContext.CurrentContext.Result.Message;
+
+            switch (testStatus)
+            {
+                
+                case NUnit.Framework.Interfaces.TestStatus.Failed:
+                    LogFail($"Test has failed with message: {message}");
+                    LogScreenshot(info, screenshot);
+                    break;
+                case NUnit.Framework.Interfaces.TestStatus.Skipped:
+                    Loginfo($"Test skipped because:{message}");
+                    break;
+                case NUnit.Framework.Interfaces.TestStatus.Passed:
+                    Loginfo("Test has passed succesfully");
+                    break;
+                default:
+                    break;
+            }
         }
     }
 }
